93cd9c4e5b520b4a2dcb3496619cb07b92a62f89
asfian99 <mk.asfian@gmail.com>
2021-01-19 23:16:40 +0700

    new column on bills table & add cloudinary upload provider

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,13 @@
-module.exports = {
+module.exports = ({ env }) => ({
   graphql: {
     playgroundAlways: true,
   },
-};
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+    },
+  },
+});
